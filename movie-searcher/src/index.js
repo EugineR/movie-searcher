aa795e7ac0ccb7dfdac15c532c449be5138c66d3
Eugene Reshentyak <euginer96@gmail.com>
2024-04-12 16:21:32 +0200

    feat: Replaced with the new app based on latest documentation

--- movie-searcher/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import Game from './components/game';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
